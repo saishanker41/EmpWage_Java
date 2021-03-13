66eb428142233e418ae01e43a96d18926560e850
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-13 17:40:18 +0530

    [sai] Daily wages added

--- EmpWageComputation_Java.java
+++ EmpWageComputation_Java.java
@@ -2,13 +2,22 @@ public class EmpWageComputation_Java {
     public static void main(String args[]) {
 
        // Constants
+
        int IS_FULL_TIME = 1;
+       int EMP_RATE_PER_HOUR = 20;
+
+       // Variables
+
+       int empHrs = 0;
+       int empWage = 0;
 
        //Computation
        double empCheck = Math.floor(Math.random() * 10) % 2;
        if (empCheck == IS_FULL_TIME)
-           System.out.println("Employee is Present");
+           empHrs = 8;
        else
-           System.out.println("Employee is Absent");
+           empHrs = 0;
+       empWage = empHrs * EMP_RATE_PER_HOUR;
+       System.out.println("Emp Wage:" + empWage);
      }
  }
