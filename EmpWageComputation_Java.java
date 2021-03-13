85db3c88ec0a386191403a7cb5b4a8b85474ff3f
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-13 17:29:47 +0530

    [sai] Check whether the Employee present or absent using random

---
+++ EmpWageComputation_Java.java
@@ -0,0 +1,14 @@
+public class EmpWageComputation_Java {
+    public static void main(String args[]) {
+
+       // Constants
+       int IS_FULL_TIME = 1;
+
+       //Computation
+       double empCheck = Math.floor(Math.random() * 10) % 2;
+       if (empCheck == IS_FULL_TIME)
+           System.out.println("Employee is Present");
+       else
+           System.out.println("Employee is Absent");
+     }
+ }
