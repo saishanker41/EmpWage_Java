77c313f18e40fc737613b6b32aec192da889062d
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-14 11:11:42 +0530

    [sai] Calculated wages for a month

--- EmpWageComputation_Java.java
+++ EmpWageComputation_Java.java
@@ -2,6 +2,7 @@ public class EmpWageComputation_Java {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
 
     public static void main(String args[]) {
 
@@ -9,24 +10,29 @@ public class EmpWageComputation_Java {
 
        int empHrs = 0;
        int empWage = 0;
+       int totalEmpWage = 0;
 
        //Computation
-       int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-       switch (empCheck) {
+       for  (int day = 0; day < NUM_OF_WORKING_DAYS; day++) 
+        {
+             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+             switch (empCheck) {
 
-               case IS_PART_TIME:
-                   empHrs = 4;
-                   break;
+                 case IS_PART_TIME:
+                     empHrs = 4;
+                     break;
 
-               case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-               default:
-                    empHrs = 0; 
+                 case IS_FULL_TIME:
+                     empHrs = 8;
+                     break;
+                 default:
+                     empHrs = 0; 
 
-       }
+             }
 
-       empWage = empHrs * EMP_RATE_PER_HOUR;
-       System.out.println("Emp Wage:" + empWage);
+             empWage = empHrs * EMP_RATE_PER_HOUR;
+             totalEmpWage += empWage;
+             System.out.println("Emp Wage:" + empWage);
+        }
      }
  }
