870d5640db676b6151ff5fc5d3827f36da3ef421
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-15 01:50:43 +0530

    [sai] Refactor the code to write a class method to compute Employee wage

--- EmpWageComputation_Java.java
+++ EmpWageComputation_Java.java
@@ -5,7 +5,7 @@ public class EmpWageComputation_Java {
     public static final int NUM_OF_WORKING_DAYS = 2;
     public static final int MAX_HRS_IN_MONTH = 10;
 
-    public static void main(String args[]) {
+    public static int computeEmpWage(){
 
        // Variables
 
@@ -40,5 +40,10 @@ public class EmpWageComputation_Java {
 
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage: " + totalEmpWage);
+        return totalEmpWage;
      }
- }
+
+     public static void main(String[] args) {
+        computeEmpWage();
+     }
+}
